58652904b9b382f35d831e5270a52aa10c5e7790
Marko Mihajlović <markomihajlovicmare01@gmail.com>
2025-12-26 01:12:34 +0100

    ICS-OpenVPN small adaptations and adjustments

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,8 +10,14 @@ pluginManagement {
 
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
     repositories {
         google()
         mavenCentral()
